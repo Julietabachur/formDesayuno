dba39140d698851a691f6c5277f34c622fec8fcd
Julieta Bachur <julietabachur@hotmail.com>
2022-05-22 23:21:29 -0300

    casi listo

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ let btnguardar = document.getElementById("btnguardar")
 let btnListo = document.getElementById("desayunoListo")
 let btnComencemos = document.getElementById("btnComencemos")
 let formularioDesayuno = document.getElementById("formDesayuno")
-let carrito = document.querySelector(".carrito")
 let parrafosMain = document.querySelector(".mainP")
 let main = document.getElementById("main")
 let mainH1 = document.getElementById("mainH1")
 let contenedorGral = document.getElementById("contenedorGral")
+let productos;
 
 
 
@@ -26,53 +26,6 @@ function crearUsuario() {
   mostrarFormDesayuno()
 }
 
-
-function mostrarFormDesayuno() {
-  //creo formulario para armar desayuno
-  formUsuario.classList.replace("d-flex", "d-none")
-  const nombreCompleto = document.getElementById("nombreCompleto").value 
-  let formDesayuno= document.getElementById("formDesayuno")
-  formDesayuno.innerHTML=   `<h3>Hola ${nombreCompleto}!</h3> 
-                            <h3>Ya podés armar tu desayuno</h3>
-                            <p>A continuacion verás las opciones disponibles</p>`
-  //creo divdulce en donde appendo el select 
-  let divOpciones = document.createElement("div")
-  divOpciones.innerHTML = `<p class="form-label fs-3 text">Elige una porcion de torta</p>
-                          <select id="selectDulce" name="dulce"></select>
-                          <p class="form-label fs-3 text">Elige algo salado!</p>
-                          <select id="selectSalado" name="salado"></select>
-                          <p class="form-label fs-3 text">Elige algo para tomar</p>
-                          <select id="selectBebida" name="bebida"></select>
-                          <p class="form-label fs-3 text">Te gustaría incluir un regalo?</p>
-                          <select id="selectRegalo" name="regalo"></select>
-                          <div class="d-flex flex-wrap row align-content-center row justify-content-center">
-                          <input class="btn" type="submit" value="Guardar" id="desayunoListo">
-                          </div>`
-  formDesayuno.appendChild(divOpciones)
-
-  //hago fetch de productos.json
-fetch('productos.json')
-.then((res) => res.json())
-.then(data => {
-   //uso un forEach para crear un option por cada producto dentro del array y las appendo a los select de arriba
-  let productos = data ;
-  console.log(productos);
-  productos.forEach(item => {
-    let option = document.createElement("option");
-    option.innerText = `${item.nombre}: ${item.descripcion} a $${item.precio}`;
-    option.value = productos.indexOf(item)
-    for (value in item) {
-      item[value] === 'dulce' ? selectDulce.appendChild(option) : null;
-      item[value] === 'salado' ? selectSalado.appendChild(option) : null;
-      item[value] === 'bebida' ? selectBebida.appendChild(option) : null;
-      item[value] === 'regalo' ? selectRegalo.appendChild(option) : null;
-    }
-  })
-})
-}
- 
-
-
 function validarCampos() {
   let camposCompletos = (document.getElementById("nombreCompleto").value  != "" &&
   document.getElementById("dni").value > 0 && 
@@ -81,6 +34,7 @@ function validarCampos() {
 
   camposCompletos ?  crearUsuario() : swal("ERROR!", "Revise que los campos se hayan completado correctamente y vuelva a intentarlo", "error");
 }
+ 
 
 //funcion para validar datos de usuario
 function ejecutarFormulario(e) {
@@ -88,7 +42,50 @@ function ejecutarFormulario(e) {
   validarCampos()
   }
 
-function crearDesayuno(e, productos) {
+  function mostrarFormDesayuno() {
+    //creo formulario para armar desayuno
+    formUsuario.classList.replace("d-flex", "d-none")
+    const nombreCompleto = document.getElementById("nombreCompleto").value 
+    let formDesayuno= document.getElementById("formDesayuno")
+    formDesayuno.innerHTML=   `<h3>Hola ${nombreCompleto}!</h3> 
+                              <h3>Ya podés armar tu desayuno</h3>
+                              <p>A continuacion verás las opciones disponibles</p>`
+    //creo divOpciones en donde appendo los select 
+    let divOpciones = document.createElement("div")
+    divOpciones.innerHTML = `<p class="form-label fs-3 text">Elige una porcion de torta</p>
+                            <select id="selectDulce" name="dulce"></select>
+                            <p class="form-label fs-3 text">Elige algo salado!</p>
+                            <select id="selectSalado" name="salado"></select>
+                            <p class="form-label fs-3 text">Elige algo para tomar</p>
+                            <select id="selectBebida" name="bebida"></select>
+                            <p class="form-label fs-3 text">Te gustaría incluir un regalo?</p>
+                            <select id="selectRegalo" name="regalo"></select>
+                            <div class="d-flex flex-wrap row align-content-center row justify-content-center">
+                            <input class="btn" type="submit" value="Guardar" id="desayunoListo">
+                            </div>`
+    formDesayuno.appendChild(divOpciones)
+  
+    //hago fetch de productos.json
+  fetch('productos.json')
+  .then((res) => res.json())
+  .then(data => {
+     //uso un forEach para crear un option por cada producto dentro del array y las appendo a los select de arriba
+    productos = data ;
+    productos.forEach(producto => {
+      let option = document.createElement("option");
+      option.innerText = `${producto.nombre}: ${producto.descripcion} a $${producto.precio}`;
+      option.value = productos.indexOf(producto)
+      for (value in producto) {
+        producto[value] === 'dulce' ? selectDulce.appendChild(option) : null;
+        producto[value] === 'salado' ? selectSalado.appendChild(option) : null;
+        producto[value] === 'bebida' ? selectBebida.appendChild(option) : null;
+        producto[value] === 'regalo' ? selectRegalo.appendChild(option) : null;
+      }
+    })
+  })
+  }
+
+function crearDesayuno(e) {
   e.preventDefault() 
   let eleccionDulce = productos[selectDulce.value]
   let eleccionSalado = productos[selectSalado.value]
@@ -98,20 +95,44 @@ function crearDesayuno(e, productos) {
   desayuno.push(new Item (eleccionSalado, 1))
   desayuno.push(new Item (eleccionBebida, 1))
   desayuno.push(new Item (eleccionRegalo, 1))
+  localStorage.setItem (user.nombreCompleto, JSON.stringify(desayuno))
   swal("HECHO!", "Tu desayuno está listo!", "success");
-  console.log(desayuno)
 
   let barraLateral = document.createElement("div")
-  barraLateral.innerHTML =  `<h3>Carrito</h3>
-                            <p>Tu desayuno contiene:</p>
-                            <table>
-                              <tr>
-                                <th>Producto</th>
-                                <th>Cantidad</th>
-                                <th>Precio</th>
-                              </tr>
+  contenedorGral.classList.add("row")
+  formularioDesayuno.classList.add("col-8")
+  barraLateral.classList.add("col-4")
+  barraLateral.innerHTML =  `<h3>Tu desayuno contiene:</h3>
+                            <table class="table table-dark table-hover">
+                              <thead>
+                                <tr>
+                                  <th scope="col">Producto</th>
+                                  <th scope="col">Cantidad</th>
+                                  <th scope="col">Precio</th>
+                                </tr>
+                              </thead>
+                              <tbody class="tbody">                                
+                              </tbody>
                             </table>`
   contenedorGral.appendChild(barraLateral)
+
+  desayuno.forEach(item =>{
+    let row = document.createElement('tr')
+    row.innerHTML=   `<td>${item.producto.nombre}</td>
+                      <td>${item.cantidad}</td>                      
+                      <td>$${item.producto.precio}</td>` 
+                      
+    let tbody = document.querySelector(".tbody")   
+    tbody.appendChild(row)                      
+  })
+
+  let rowTotal = document.createElement('tr')  
+  let tbody = document.querySelector(".tbody")
+  let total = desayuno.reduce((total, item) => total + item.producto.precio * item.cantidad, 0);
+  rowTotal.innerHTML = `<td>TOTAL:</td>
+                        <td></td>                      
+                        <td>$${total}</td>`
+  tbody.appendChild(rowTotal)
 }
 
 //evento para ver formulario de usuario
@@ -125,9 +146,3 @@ btnComencemos.addEventListener("click", ()=>{
 formUsuario.addEventListener("submit", ejecutarFormulario);
 formularioDesayuno.addEventListener("submit", crearDesayuno);
 
-// funcion precio del desayuno
-// let preciofinal = () => {
-//   const precio = totalDesayuno.reduce( (acc, item) => {return acc += item }, 0 )  
-//   let total = document.createElement("p")
-//   total.innerText = "El valor total de su desayuno es: $"+ precio
-//   divTotal.appendChild(total)}
